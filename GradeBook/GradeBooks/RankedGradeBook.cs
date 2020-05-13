1e5cf17dd369fa8d6313abc664a2006c2b2b64a5
goranstepanovic89 <goran.stepanovic89@gmail.com>
2020-05-13 23:00:33 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,32 @@
 using GradeBook.Enums;
+using System;
+
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+        int index = 5;
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count <= 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
